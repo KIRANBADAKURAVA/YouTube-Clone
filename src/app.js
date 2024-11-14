e2debb51db333d11b7199b857984715e890358c3
KIRANBADAKURAVA <kiranbadakurava@gmail.com>
2024-11-14 21:45:33 +0530

    Server setup

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+// cors 
+app.use(cors({
+    options: process.env.FRONTEND_URL,
+    credentials: true
+}))
+
+//json 
+app.use(express.json({
+    limit: '16kb'
+}))
+
+//url
+
+app.use(express.urlencoded({
+    extended:true,
+    limit: '16kb'
+}))
+
+
+//cache
+app.use(express.static('public'))
+
+app.use(cookieParser())
+
+export default app
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import connectDB from "./db/indexdb.js";
 import dotenv from 'dotenv'
+import app from "./app.js";
 
 dotenv.config({
     path: './env'
 })
-connectDB();
+connectDB()
+.then(()=>{
+   
+    app.listen(process.env.PORT,()=>{
+        console.log('listioning ', process.env.PORT);
+    } )
+   
+})
+.catch((error )=>(console.error('Error in conneting server at intdex.js file ',   error)))
